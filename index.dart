3971f8d13e2958e3acada5f71a15fdf0962908f3
David Gaspar <davidgaspar.dev@gmail.com>
2018-07-01 23:50:52 +0000

    Started HTTP Server with dart programming language

--- index.dart
+++ index.dart
@@ -1,3 +1,14 @@
-main() {
-  print('Hello World!');
+import 'dart:io';
+
+main() async {
+  
+  var server = await HttpServer.bind(InternetAddress.ANY_IP_V4, 80);
+  print("Serving at ${server.address}:${server.port}");
+
+  await for (var request in server) {
+    request.response
+      ..headers.contentType = new ContentType("text", "plain", charset: "utf-8")
+      ..write('Hello, world!')
+      ..close();
+  }
 }
