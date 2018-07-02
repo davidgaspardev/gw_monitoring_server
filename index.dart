870793fb7e005c85ad382b88de1814aac2b07458
David Gaspar <davidgaspar.dev@gmail.com>
2018-07-02 05:14:36 +0000

    Query parameters added on HTTP server

--- index.dart
+++ index.dart
@@ -1,14 +1,25 @@
 import 'dart:io';
 
 main() async {
-  
+
   var server = await HttpServer.bind(InternetAddress.ANY_IP_V4, 80);
-  print("Serving at ${server.address}:${server.port}");
+  print("Serving at ${server.address.host}:${server.port}");
 
   await for (var request in server) {
-    request.response
-      ..headers.contentType = new ContentType("text", "plain", charset: "utf-8")
-      ..write('Hello, world!')
-      ..close();
+    var response = request.response;
+
+    if(request.method == 'GET') {
+
+      if(request.uri.hasQuery) print('Address: ${request.uri.queryParameters['addr']} Port: ${request.uri.queryParameters['port']}');
+
+      response
+        ..headers.contentType = new ContentType("text", "plain", charset: "utf-8")
+        ..write('Hello, world!\n');
+
+
+    } else response.statusCode = HttpStatus.METHOD_NOT_ALLOWED;
+
+    response.close();
+
   }
 }
