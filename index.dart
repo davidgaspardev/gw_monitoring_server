827f40c411fb8e629a1560c2b3fb0c076c302204
da052556 <david.gaspar@intelbras.com.br>
2018-07-02 15:19:17 -0300

    Added file system

--- index.dart
+++ index.dart
@@ -2,19 +2,34 @@ import 'dart:io';
 
 main() async {
 
-  var server = await HttpServer.bind(InternetAddress.ANY_IP_V4, 80);
+  var server = await HttpServer.bind(InternetAddress.ANY_IP_V4, 8080);
   print("Serving at ${server.address.host}:${server.port}");
 
   await for (var request in server) {
-    var response = request.response;
+    var response = request.response,
+        uri      = request.uri;
 
     if(request.method == 'GET') {
 
-      if(request.uri.hasQuery) print('Address: ${request.uri.queryParameters['addr']} Port: ${request.uri.queryParameters['port']}');
+      if(uri.hasQuery) {
 
-      response
-        ..headers.contentType = new ContentType("text", "plain", charset: "utf-8")
-        ..write('Hello, world!\n');
+        if(uri.queryParameters['mac'] != null) {
+
+          var file = new File('${uri.queryParameters['mac']}.log');
+          var sink = file.openWrite(mode: FileMode.APPEND);
+
+          var msg = '[ MAC: ${uri.queryParameters['mac']} |';
+          msg += ' Date: ${new DateTime.now()} |';
+          msg += uri.queryParameters['addr'] == null ? '' : ' Address: ${uri.queryParameters['addr']} |';
+          msg += uri.queryParameters['port'] == null ? '' : ' SIP Port${uri.queryParameters['port']}';
+          msg += ' ]\n';
+
+          sink.write(msg);
+
+          sink.close();
+        }
+
+      }
 
 
     } else response.statusCode = HttpStatus.METHOD_NOT_ALLOWED;
